de6bdf8eda9affa8983917bf129a9ef319ed5f6b
emabytes <emadavor22@gmail.com>
2020-07-13 01:27:48 +0200

    clear warning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 // Aufgabenstellung
 // In dieser Web-App möchten wir ein Semi-Horoskop entsprechend des eingegebenen Geburtsmonats erstellen. Es wird nur auf den Monat bezogen.
 
+function clearWarn() {
+    document.getElementById("warning").innerHTML = "";
+}
+
 function showHoroscope() {
   let monthInput = document.getElementById("month-input").value;
 
@@ -16,7 +20,7 @@ function showHoroscope() {
     document.getElementById("sign-img").src =
       "https://img.icons8.com/carbon-copy/100/000000/capricorn.png";
     document.getElementById("text").innerHTML =
-      "You don't have to dive into the deep end of the pool to learn how to swim, so test the waters; go slow and have fun! Hop on the elevator without checking the date of the last inspection.";
+        "You don't have to dive into the deep end of the pool to learn how to swim, so test the waters; go slow and have fun! Hop on the elevator without checking the date of the last inspection.";
   } else if (
     monthInput == "february" ||
     monthInput == "February" ||
@@ -29,7 +33,7 @@ function showHoroscope() {
     document.getElementById("sign-img").src =
       "https://img.icons8.com/carbon-copy/100/000000/aquarius.png";
     document.getElementById("text").innerHTML =
-      "Challenging the structure of normal life is something you do either passively or aggressively. Live the adventure and explore the Circle of Life during the upcoming weeks. Eat breakfast at Midnight, and have high tea at 3am.";
+        "Challenging the structure of normal life is something you do either passively or aggressively. Live the adventure and explore the Circle of Life during the upcoming weeks. Eat breakfast at Midnight, and have high tea at 3am.";
   } else if (
     monthInput == "march" ||
     monthInput == "March" ||
@@ -41,7 +45,7 @@ function showHoroscope() {
     document.getElementById("sign-img").src =
       "https://img.icons8.com/carbon-copy/100/000000/pisces.png";
     document.getElementById("text").innerHTML =
-      "Compassion is good, but a Pisces tries to bear the burden of the world on his back. A close acquaintance will come to you for advice very soon, and it may help if you keep that iPod playing in one ear while you hear him out.";
+        "Compassion is good, but a Pisces tries to bear the burden of the world on his back. A close acquaintance will come to you for advice very soon, and it may help if you keep that iPod playing in one ear while you hear him out.";
   } else if (
     monthInput == "april" ||
     monthInput == "April" ||
@@ -53,7 +57,7 @@ function showHoroscope() {
     document.getElementById("sign-img").src =
       "https://img.icons8.com/carbon-copy/100/000000/aries.png";
     document.getElementById("text").innerHTML =
-      "A mutually beneficial opportunity will be presented to you by someone with whom you have butted horns in the past. Ease up, my horny friend. Learn to use your beguiling charm and wit to its best advantage.";
+        "A mutually beneficial opportunity will be presented to you by someone with whom you have butted horns in the past. Ease up, my horny friend. Learn to use your beguiling charm and wit to its best advantage.";
   } else if (monthInput == "may" || monthInput == "May" || monthInput == "5") {
     document.getElementById("sign").innerHTML = "Your horoscope sign is Taurus";
     document.getElementById("sign-img").src =
@@ -71,7 +75,7 @@ function showHoroscope() {
     document.getElementById("sign-img").src =
       "https://img.icons8.com/carbon-copy/100/000000/gemini.png";
     document.getElementById("text").innerHTML =
-      "Lucky you! This birth sign is the only venue that portrays multiple personalities as highly desirable and socially acceptable. You are blessed with the Yin and the Yang of the Universe, if only you can bring them into balance more often.";
+        "Lucky you! This birth sign is the only venue that portrays multiple personalities as highly desirable and socially acceptable. You are blessed with the Yin and the Yang of the Universe, if only you can bring them into balance more often.";
   } else if (
     monthInput == "july" ||
     monthInput == "July" ||
@@ -83,7 +87,7 @@ function showHoroscope() {
     document.getElementById("sign-img").src =
       "https://img.icons8.com/carbon-copy/100/000000/cancer.png";
     document.getElementById("text").innerHTML =
-      "The Crab is known as a sensitive, intuitive birth sign. Instead of letting your energies and abilities be covered up with a bushel basket, crawl out into the light and show your amazing personality to the world.";
+        "The Crab is known as a sensitive, intuitive birth sign. Instead of letting your energies and abilities be covered up with a bushel basket, crawl out into the light and show your amazing personality to the world.";
   } else if (
     monthInput == "august" ||
     monthInput == "August" ||
@@ -95,7 +99,7 @@ function showHoroscope() {
     document.getElementById("sign-img").src =
       "https://img.icons8.com/carbon-copy/100/000000/leo.png";
     document.getElementById("text").innerHTML =
-      "Leave the hunt to others and take time to enjoy your environment. Leos are known for their benevolent natures and leadership. Look for help from a modest, unassuming little mouse that you know.";
+        "Leave the hunt to others and take time to enjoy your environment. Leos are known for their benevolent natures and leadership. Look for help from a modest, unassuming little mouse that you know.";
   } else if (
     monthInput == "september" ||
     monthInput == "September" ||
@@ -107,7 +111,7 @@ function showHoroscope() {
     document.getElementById("sign-img").src =
       "https://img.icons8.com/carbon-copy/100/000000/virgo.png";
     document.getElementById("text").innerHTML =
-      "Your tendency to be a bit obsessive -compulsive in daily life can interfere with your best traits and make it difficult for people to see what a wonderful person you really are. Touch a goldfish, frolic in the leaves and be relaxed enough to double dip that Nacho Chip.";
+        "Your tendency to be a bit obsessive -compulsive in daily life can interfere with your best traits and make it difficult for people to see what a wonderful person you really are. Touch a goldfish, frolic in the leaves and be relaxed enough to double dip that Nacho Chip.";
   } else if (
     monthInput == "october" ||
     monthInput == "October" ||
@@ -119,7 +123,7 @@ function showHoroscope() {
     document.getElementById("sign-img").src =
       "https://img.icons8.com/carbon-copy/100/000000/libra.png";
     document.getElementById("text").innerHTML =
-      "If you are planning to spend the weekend rearranging your silverware drawer you may be past the point of change regarding any rehabilitative efforts. Still you should make the effort to break out of your self-imposed box and learn to experience the full range of life's adventures.";
+        "If you are planning to spend the weekend rearranging your silverware drawer you may be past the point of change regarding any rehabilitative efforts. Still you should make the effort to break out of your self-imposed box and learn to experience the full range of life's adventures.";
   } else if (
     monthInput == "november" ||
     monthInput == "November" ||
@@ -132,7 +136,7 @@ function showHoroscope() {
     document.getElementById("sign-img").src =
       "https://img.icons8.com/carbon-copy/100/000000/scorpio.png";
     document.getElementById("text").innerHTML =
-      "Sensuous, sexy, seductive and secretive all are words that describe the most passionate of all birth signs-the Scorpio. The downside to all of the exotic, alluring qualities a Scorpio possesses is their vengeful, jealous nature.";
+        "Sensuous, sexy, seductive and secretive all are words that describe the most passionate of all birth signs-the Scorpio. The downside to all of the exotic, alluring qualities a Scorpio possesses is their vengeful, jealous nature.";
   } else if (
     monthInput == "december" ||
     monthInput == "December" ||
@@ -145,7 +149,7 @@ function showHoroscope() {
     document.getElementById("sign-img").src =
       "https://img.icons8.com/carbon-copy/100/000000/sagittarius.png";
     document.getElementById("text").innerHTML =
-      "With your happy go lucky nature and eagerness for adventure you are never at a loss for friends. However certain events in the near future could cause you to behave more like fellow Sagittarian, Britney Spears during her worst meltdown moments.";
+        "With your happy go lucky nature and eagerness for adventure you are never at a loss for friends. However certain events in the near future could cause you to behave more like fellow Sagittarian, Britney Spears during her worst meltdown moments.";
   } else {
     document.getElementById("warning").innerHTML =
       "You forgot to enter your month of birth. Try again! ";
